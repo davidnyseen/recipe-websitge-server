9bd41bded1d319b0c00ceb7c02bbf14c2db714c0
Isaac Svi <yitzchaksviridyuk@gmail.com>
2022-01-06 12:58:16 +0200

    added line to parse json

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-var http = require('https');
 const express = require('express');
-// var cors = require('cors');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+
 const app = express();
-var cors = require('cors');
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions))
-// fetch-polyfill.js
-const fetch = require('node-fetch')
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.post('/', (req, res) => { 
-  console.log(req.body);
-  fetch("https://api.edamam.com/search?app_id=c08ba36f&app_key=2e5c98200b0dd0211ff9f285f249efb6&q=pizza")
-  .then(response => response.json())
-  .then(data => {
-    res.send(data)})
+app.post('/', (req, res) => {
+    console.log(req.body);
+    fetch(
+        'https://api.edamam.com/search?app_id=c08ba36f&app_key=2e5c98200b0dd0211ff9f285f249efb6&q=pizza'
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            res.send(data);
+        });
 });
 
-app.listen(5000); //  i am a server
+app.listen(5000, () => console.log('Server running on port 5000')); //  i am a server
