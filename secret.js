131a31c8c23214fbdf87b6aca6e41e767dc585de
david nyssen <76433487+davidnyseen@users.noreply.github.com>
2022-02-20 18:18:53 +0200

    Delete secret.js

--- secret.js
+++
@@ -1,8 +0,0 @@
-exports.secrets = {
-    'dbURI' : 'mongodb+srv://david:alisacara1@cluster0.fddex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    'awsBucketName' : 'recipe-website-bucket',
-    'secret' : 'david nyssen secret',
-    'awsRegion' : 'us-east-1',
-    'awsAccessCode' : 'AKIASSWOLEFGCU6UGBZS',
-    'awsSecretAccessCode' : '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'
-}
