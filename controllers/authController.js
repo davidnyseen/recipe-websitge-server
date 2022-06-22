8bb3b42123f25515cf18c1880b3d1fe392fdf532
david nyssen <davidnyssen22@gmail.com>
2022-06-22 17:24:42 +0300

    a

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,7 +64,10 @@ module.exports.login_post = async (req, res) => {
     const user = await User.login(email, password);
     console.log(user);
     const token = createToken(user._id);
-
+    res.header(`Access-Control-Allow-Origin: ${process.env.WEBSITE_URL}`);
+    res.header("Access-Control-Allow-Credentials: true");
+    res.header("Access-Control-Allow-Methods: GET, POST");
+    res.header("Access-Control-Allow-Headers: Content-Type, *");
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000, path:"/", secure: true, proxy : true,
   });
     res.status(200).json({ user: user.name, id: user._id, recommendations: user.recommendationSet });
