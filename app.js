e53d94fd249d3ff06dcf01c95e9613008f3d2208
david nyssen <davidnyssen22@gmail.com>
2022-06-24 17:31:44 +0300

    a

--- app.js
+++ app.js
@@ -21,13 +21,11 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
-app.use('*', (req, res, next) => {
-  res.header(`Access-Control-Allow-Origin: https://recipe-website-server.herokuapp.com/`);
-  res.header("Access-Control-Allow-Credentials: true");
-  res.header("Access-Control-Allow-Methods: GET, POST");
-  res.header("Access-Control-Allow-Headers: Content-Type, *");
-  next()
-})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // view engine
 
 app.use(cors(corsOptions));
